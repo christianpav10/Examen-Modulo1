c5a7a1573bf7acf7825faba3a6a7a3b49c2ffe62
Christian <crica10andravilla@hotmail.com>
2020-10-31 11:33:21 -0500

    add substractNumbers function and test

--- src/test/java/Christian/Andrade/NumberTest.java
+++ src/test/java/Christian/Andrade/NumberTest.java
@@ -11,4 +11,10 @@ private Number number;
         this.number=new Number(4,5);
         assertEquals(9,this.number.addNumbers());
     }
+    @Test
+    public void substractNumbers(){
+        this.number=new Number (2,2);
+        assertEquals(0,this.number.substractNumbers());
+
+    }
 }
